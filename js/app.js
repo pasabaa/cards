6af73fde190e66b1ea0c9c85c57834babf5644ee
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2022-03-30 18:35:53 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+let btnSunmit = document.querySelector('#btnSubmit');
+var cardColor = document.querySelector('#card-change-color');
+
+btnSunmit.addEventListener('click', () => {
+    var color = document.querySelector('#color').value;
+    changeColor(color);
+});
+
+function changeColor (color) {
+
+    if(cardColor.classList.contains('bg-primary')){
+
+        cardColor.classList.replace('bg-primary', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-secondary')) {
+
+        cardColor.classList.replace('bg-secondary', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-success')) {
+
+        cardColor.classList.replace('bg-success', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-info')) {
+
+        cardColor.classList.replace('bg-info', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-warning')) {
+
+        cardColor.classList.replace('bg-warning', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-danger')) {
+
+        cardColor.classList.replace('bg-danger', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-light')) {
+
+        cardColor.classList.replace('bg-light', `bg-${color}`);
+
+    } else if(cardColor.classList.contains('bg-dark')) {
+
+        cardColor.classList.replace('bg-dark', `bg-${color}`);
+        
+    }
+
+}
+
